9198672fd6480da57d9488d71b0e6e6cb4e6ef3b
sena <hargiantofebrisena@gmail.com>
2022-07-26 21:57:37 +0700

    initial

--- go.mod
+++ go.mod
@@ -6,7 +6,9 @@ require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/gofiber/fiber/v2 v2.35.0 // indirect
+	github.com/gofiber/jwt/v2 v2.2.7 // indirect
 	github.com/gofiber/utils v0.0.10 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
 	github.com/gorilla/schema v1.1.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
--- main.go
+++ main.go
@@ -3,13 +3,60 @@ package main
 import (
 	config "goFiber/config"
 	dog "goFiber/controller/dog"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	jwtware "github.com/gofiber/jwt/v2"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 func main() {
 	app := fiber.New()
 	config.Connect()
+
+	app.Post("/login", login)
+
 	app.Get("/", dog.GetDogs)
+
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: []byte("secret"),
+	}))
+
+	app.Get("/restricted", restricted)
+
 	app.Listen(":3000")
+}
+
+func login(c *fiber.Ctx) error {
+	user := c.FormValue("user")
+	pass := c.FormValue("pass")
+
+	if user != "john" || pass != "doe" {
+		return c.SendStatus(fiber.StatusUnauthorized)
+	}
+
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+	claims["name"] = "John Doe"
+	claims["admin"] = true
+	claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+	t, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return c.JSON(fiber.Map{"token": t})
+}
+
+func accessible(c *fiber.Ctx) error {
+	return c.SendString("Accessible")
+}
+
+func restricted(c *fiber.Ctx) error {
+	user := c.Locals("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	name := claims["name"].(string)
+	return c.SendString("Welcome " + name)
 }
