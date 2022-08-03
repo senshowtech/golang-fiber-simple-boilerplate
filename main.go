087f9fb1ef069861eda97e15c839d03ec63c01fc
sena <hargiantofebrisena@gmail.com>
2022-08-03 23:13:06 +0700

    initial

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	config "goFiber/config"
 	auth "goFiber/controller/auth"
-	dog "goFiber/controller/dog"
+	dogController "goFiber/controller/dog"
 	middleware "goFiber/middlewares"
 
 	"github.com/gofiber/fiber/v2"
@@ -13,7 +13,7 @@ func main() {
 	app := fiber.New()
 	config.Connect()
 	app.Post("/login", auth.Login)
-	app.Get("/", dog.GetDogs)
+	app.Get("/", dogController.GetDogs)
 	app.Get("/restricted", middleware.AuthRequired(), auth.Restricted)
 	app.Listen(":5000")
 }
